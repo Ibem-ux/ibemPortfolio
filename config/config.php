38bd295800873d3e039e189db3ff6d7719ad2035
Ibem <cnhovem@gmail.com>
2025-11-13 16:40:20 +0800

    wasmer

--- config/config.php
+++ config/config.php
@@ -1,10 +1,11 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "db.fr-pari1.bengt.wasmernet.com";
+$port = "10272";
+$username = "59895bc873858000216ecd8c794d";
+$password = "06915989-5bc8-7520-8000-7d1cb07beccf";
 $dbname = "ibem_db";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, port: $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
