acbc5577642c48280b0ab6188cf82e575442c2cd
Ibem <cnhovem@gmail.com>
2025-11-14 10:09:13 +0800

    dizSit

--- config/config.php
+++ config/config.php
@@ -1,10 +1,11 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "ibem_db";
+$servername = "db.fr-pari1.bengt.wasmernet.com";
+$port = "10272";
+$username = "68eb28337e02800042f6cf96fc53";
+$password = "069168eb-2834-709b-8000-4ba9cc27c819";
+$dbname = "ibem_db2";
 
-$conn = new mysqli($servername, $username, $password, $dbname,);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
--- config/connect.php
+++ config/connect.php
@@ -1,10 +1,11 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "login";
+$host = "db.fr-pari1.bengt.wasmernet.com";
+$port ="10272";
+$user = "68eb28337e02800042f6cf96fc53";
+$pass = "069168eb-2834-709b-8000-4ba9cc27c819";
+$db = "ibem_db2";
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db , $port);
 if ($conn->connect_error) {
     die("Failed to connect DB: " . $conn->connect_error);
 }
